169bdad0c7d630b41c8d07971d4bc097b2d44f82
龙 陈 <chenspromise@qq.com>
2020-10-22 15:04:20 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGeneric<string> test = new MyGeneric<string>("这是自定义泛型");
+
+            Console.WriteLine("Hello World!");
+
+            Func<string, string> func = num => { return "num is " + num; };
+
+            Show<string, Func<string, string>>("AAAAAAAAAAAAAAAAAA", ShowG);
+
+            Show<int, string>(1111111, null);
+        }
+
+
+        public static G ShowG<G>(G g)
+        {
+            return g;
+        }
+
+        public static void Show<T, G>(T t, Func<G, G> f)
+        {
+            Console.WriteLine(t.ToString());
+        }
+    }
+
+
+    class MyGeneric<T> : List<T>
+    {
+        private T t;
+
+        public MyGeneric(T t)
+        {
+            this.t = t;
+        }
+    }
+}
